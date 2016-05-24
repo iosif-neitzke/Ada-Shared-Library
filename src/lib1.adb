30c19ad67f68d4c2da75aa3d8110ad1793cde203
Iosif Neitzke <iosif.neitzke@googlemail.com>
2016-05-24 18:57:14 -0500

    Started example project.

---
+++ src/lib1.adb
@@ -0,0 +1,7 @@
+package body lib1 is
+function stuff return Integer is
+begin
+      return 1;
+   end stuff;
+
+end lib1;
---
+++ src/lib1.ads
@@ -0,0 +1,3 @@
+package lib1 is
+   function stuff return Integer;
+end lib1;
